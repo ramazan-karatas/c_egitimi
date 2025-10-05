b3f2160e62424db66292ebd1be27f79df0e0e1bf
RamazanKaratas <ramazankaratas626@gmail.com>
2025-10-05 20:14:53 +0300

    First program added

---
+++ gun_1/ilk_program/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+
+    // tek satırlı yorum satırı
+    /*
+     * çok satırlı
+     * yorum satırı
+     */
+    
+    printf("Merhaba dÜNYA!\n"); // ekrana çıktı vermemizi sağlayan kod.
+    return 0;
+}
