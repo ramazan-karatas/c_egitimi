ad6a2051d0887fb5c10952493b0614e4fd27e30b
RamazanKaratas <ramazankaratas626@gmail.com>
2025-10-05 21:22:43 +0300

    some corrections

--- gun_1/veri_tipleri/main.c
+++ gun_1/veri_tipleri/main.c
@@ -5,11 +5,13 @@ int main(void) {
     printf("Char tipi veri: %c \n", my_char); // char tipi değişkenler için %c kullanılır.
     // burada \n karakteri imleci yeni satıra geçirir.
 
-    int sayi1 = 12;
-    printf("int tipi veri: %d \n", sayi1); // sayilar için %d kullanılır.
+    short short_var = 234;
+    printf("short tipi veri: %hd \n", short_var); // short tipi değişkenler için %hd kullanılır.
 
-    short short1 = 234;
-    printf("short tipi veri: %d \n", short1);
+    int int_var = 12;
+    printf("int tipi veri: %d \n", int_var); // sayilar için %d kullanılır.
+
+
 
 
     long long_var = 2391932;
@@ -19,8 +21,8 @@ int main(void) {
     printf("float tipi veri: %f \n", float_var); // virgüllü sayılar için %f kullanılır.
 
 
-    double sayi2 = 1.32;
-    printf("double tipi veri: %f \n", sayi2);
+    double double_var = 1.32;
+    printf("double tipi veri: %lf \n", double_var); // double sayılar için %lf kullanılır.
 
     return 0;
 }
