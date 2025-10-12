dcc661c6742fdc151b4739935e1b7c31e02a11a2
RamazanKaratas <ramazankaratas626@gmail.com>
2025-10-12 19:28:05 +0300

    loops added

--- gun_2/b4_donguler/main.c
+++ gun_2/b4_donguler/main.c
@@ -3,6 +3,44 @@
 
 int main(void) {
     // donguler
+    /*
+     * c dilinde for donguleri ve while donguleri olmak uzere 2 temel dongu vardir.
+     *
+     * 1. for donguleri: belirli bir kodun belirli bir sayida calismasi icin kullanilir.
+     * for(ifade1, ifade2, ifade3) {
+     * calistirilacak kod;
+     * }
+     * ifade1: kod blogunun basinda bir kere calistirilir.
+     * ifade2: kod blogunun calismasi icin kosulu belirtir.
+     * ifade3: kod blogunun calistigi her seferden sonra calisir.
+     *
+     */
+
+    for (int a = 0; a < 10; a++) { // 0'dan 10'a kadar olan sayilari ekrana yazan dongu.
+        printf("sayac: %d\n", a);
+    }
+
+    /*
+     * while donguleri: belirli bir sartin saglanmasina bagli olarak calisan dongulerdir.
+     *
+     * while (kosul) {
+     *  kosulun saglandigi durumlarda bu kod kismi calisir.
+     * }
+     */
+
+    int sayac = 0;
+    while (sayac < 5) { // 5 kere merhaba Gazi yazan dongu.
+        printf("Merhaba Gazi\n");
+        sayac++;
+    }
+
+    // do while yapisi
+
+    int sayac2 = 0;
+    do {
+        printf("Do while dongusunden merhaba\n");
+        sayac2++;
+    } while (sayac2 < 5); // garanti olarak bir kez calisir. Sonrasinda calismasi icin kosulun saglanmasi gerekir.
     return 0;
 }
 
